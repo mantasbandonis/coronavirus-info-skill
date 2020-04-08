33212bbb684291cb297511a3a4110c85f996504d
Mantas Bandonis <43992327+mantasbandonis@users.noreply.github.com>
2020-04-08 17:39:48 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,21 @@
-from mycroft import MycroftSkill, intent_file_handler
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
 
 
-class CoronavirusInfo(MycroftSkill):
+class CoronavirusInfoSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
     @intent_file_handler('info.coronavirus.intent')
     def handle_info_coronavirus(self, message):
         self.speak_dialog('info.coronavirus')
-
+    
+    def initialize(self):
+        # TO-DO! 
+        pass
+    
+    def stop(self):
+        self.stop_beeping()
 
 def create_skill():
     return CoronavirusInfo()
