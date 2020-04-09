38dcc7a36a85dea6b225ca1ddf10b9619b23c7ec
Mantas Bandonis <43992327+mantasbandonis@users.noreply.github.com>
2020-04-09 15:41:55 +0200

    fix method errors

--- __init__.py
+++ __init__.py
@@ -1,6 +1,4 @@
-from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
-
 
 class CoronavirusInfoSkill(MycroftSkill):
     def __init__(self):
@@ -8,24 +6,24 @@ class CoronavirusInfoSkill(MycroftSkill):
         self.learning = True
         
     @intent_file_handler('situation.coronavirus.intent')
-    def handle_info_coronavirus(self, message):
+    def handle_situation_coronavirus(self, message):
         self.speak_dialog('situation.coronavirus')
     
     @intent_file_handler('symptoms.coronavirus.intent')
-    def handle_info_coronavirus(self, message):
+    def handle_symptoms_coronavirus(self, message):
         self.speak_dialog('symptoms.coronavirus')
     
     @intent_file_handler('information.coronavirus.intent')
-    def handle_info_coronavirus(self, message):
+    def handle_information_coronavirus(self, message):
         self.speak_dialog('information.coronavirus')
 
     @intent_file_handler('shopping.info.corona.intent')
-    def handle_info_coronavirus(self, message):
+    def handle_shopping_coronavirus(self, message):
         self.speak_dialog('shopping.info.corona')
      
     @intent_file_handler('news.coronavirus.intent')
-    def handle_info_coronavirus(self, message):
-        self.speak_dialog('news.coronavirus.dialog')
+    def handle_news_coronavirus(self, message):
+        self.speak_dialog('news.coronavirus')
 
 def create_skill():
     return CoronavirusInfoSkill()
