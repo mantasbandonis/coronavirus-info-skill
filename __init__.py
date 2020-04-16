e83f3361c51429690bf555551870f0f195808165
Mantas Bandonis <43992327+mantasbandonis@users.noreply.github.com>
2020-04-16 13:42:57 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
+from mycroft.util import play_mp3
 
 class CoronavirusInfoSkill(MycroftSkill):
     def __init__(self):
@@ -7,8 +8,29 @@ class CoronavirusInfoSkill(MycroftSkill):
         
     @intent_file_handler('situation.coronavirus.intent')
     def handle_situation_coronavirus(self, message):
-        self.speak_dialog('situation.coronavirus')
-    
+        #self.speak_dialog('situation.coronavirus')
+        answer_situation = self.get_response('situation.coronavirus')
+        
+        if "nachrichten" in answer_situation:
+            self.speak("Okay, ich spiele die neuen Nachrichten von Ö3 zu dem Coronavirus")
+            play_mp3("https://oe3meta.orf.at/oe3mdata/StaticAudio/Nachrichten.mp3")
+            
+        elif "einkaufen" in answer_situation:
+            answer_einkaufen = self.get_response("Möchtest du etwas zur empfohlenen Einkaufszeit wissen?")
+            
+            if "ja" in answer_einkaufen:
+                self.speak("Okay, folgendes:")
+                self.speak_dialog('shopping.info.corona')
+            
+            else:
+                answer_anders = self.get_response("Möchtest du vielleicht noch etwas anderwes wissen?")
+                
+                if "einkaufen" in answer_anders:
+                    answer_einkaufen = self.get_response("Möchtest du etwas zur empfohlenen Einkaufszeit wissen?")
+            
+                elif "nein" in answer_anders:
+                    self.speak("Okay, danke ich bin gerne für dich da", expect_response=False)
+            
     @intent_file_handler('symptoms.coronavirus.intent')
     def handle_symptoms_coronavirus(self, message):
         self.speak_dialog('symptoms.coronavirus')
